bbc1c333066b20f949bbabdfe49d33bdffcc6d8c
Luis Molina <lamolina@wisc.edu>
2021-08-19 15:51:15 -0500

    Prevent sounds from queuing infinitely, allow sounds with priority to play even when other sounds are playing.

--- src/play_sound.py
+++ src/play_sound.py
@@ -11,6 +11,7 @@ class SoundPlayer:
         pygame.mixer.init()
 
         self.assets_folder = rospy.get_param("~assets_folder", None)
+        self.priority_sounds = []
 
         if self.assets_folder is None:
             rospy.signal_shutdown("Need assets folder location")
@@ -18,17 +19,29 @@ class SoundPlayer:
 
         self.sounds = {}
         self.addSound('fail', "fail.wav")
-        self.addSound('success', "success.wav")
+        self.addSound('success', "success.wav", True)
+        self.addSound('out_of_time', "out_of_time.wav", True)
 
         self.command_sub = rospy.Subscriber("/play_sound/sound", String, self.handleSound)
 
-    def addSound(self, trigger_str, file_name):
-        self.sounds[trigger_str] = pygame.mixer.Sound(self.assets_folder + file_name)    
+    def addSound(self, trigger_str, file_name, has_priority=False):
+        self.sounds[trigger_str] = pygame.mixer.Sound(self.assets_folder + file_name)
+        if has_priority:
+            self.priority_sounds.append(trigger_str)
 
     def handleSound(self, msg: String):
-        if msg.data in self.sounds:
+        is_busy = pygame.mixer.get_busy()
+        if msg.data in self.sounds and not is_busy:
             self.sounds[msg.data].play()
 
+        # If sound has priority, wait for channel to clear
+        if is_busy and msg.data in self.priority_sounds:
+            while is_busy:
+                is_busy = pygame.mixer.get_busy()
+        
+            self.sounds[msg.data].play()
+
+        
 
 if __name__ == "__main__":
     rospy.init_node("sound_player", anonymous=True)
